4ec06a8498ce6dc308d8d64f55bd51c01254380b
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-17 15:10:11 -0500

    adjust colors in colrs.xml. Adjust test sixe in dimen.xml. Add drawables for each card. Move card creation into card generator method.

--- app/src/main/java/com/example/android/occasioncards/CardActivity.java
+++ app/src/main/java/com/example/android/occasioncards/CardActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.occasioncards;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,7 @@ public class CardActivity extends AppCompatActivity {
 
     private int cardChoice;
 
-    Card[] cards = new Card[10];
+    Card[] cards = new Card[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +33,71 @@ public class CardActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         cardChoice = intent.getIntExtra("cardChoice", 0);
+        Log.v("cardChoice: ", "" + cardChoice);
 
         generateCards();
 
         switch (cardChoice){
-            case 1: setCard(cards[0]);
-            case 2:;
-            case 3:;
-            case 4:;
-            case 5:;
-            case 6:;
-            case 7:;
-            case 8:;
-            case 9:;
-            case 10:;
-            case 11:;
+            case 1:
+                setCard(cards[0]);
+                break;
+            case 2:
+                setCard(cards[1]);
+                break;
+            case 3:
+                setCard(cards[2]);
+                break;
+            case 4:
+                setCard(cards[3]);
+                break;
+            case 5:
+                setCard(cards[4]);
+                break;
+            case 6:
+                setCard(cards[5]);
+                break;
+            case 7:
+                setCard(cards[6]);
+                break;
+            case 8:
+                setCard(cards[7]);
+                break;
+            case 9:
+                setCard(cards[8]);
+                break;
+            case 10:
+                setCard(cards[9]);
+                break;
+            case 11:
+                setCard(cards[10]);
+                break;
+            case 12:
+                setCard(cards[11]);
+                break;
         }
     }
 
-    private void generateCards(){
-        cards[0] = new Card(R.string.happy, R.string.birthday, R.drawable.donut, getResources().getColor(R.color.gradientColorEnd));
-    }
+    @Override
+    public void onBackPressed() {
+        Intent cardIntent = new Intent(CardActivity.this, MainActivity.class);
+        cardIntent.putExtra("cardChoice", 0);
+        startActivity(cardIntent);
+   }
+
+   private void generateCards(){
+       cards[0] = new Card(R.string.happy, R.string.birthday, R.drawable.donut, getResources().getColor(R.color.gradientColorEnd));
+       cards[1] = new Card(R.string.happy, R.string.year, R.drawable.newyear, getResources().getColor(R.color.colorPrimaryLight));
+       cards[2] = new Card(R.string.happy, R.string.groundhog, R.drawable.groundhog, getResources().getColor(R.color.colorPrimary));
+       cards[3] = new Card(R.string.happy, R.string.valentine, R.drawable.valentine, getResources().getColor(R.color.colorPrimaryDark));
+       cards[4] = new Card(R.string.happy, R.string.patrick, R.drawable.patrick, getResources().getColor(R.color.gradientColorEnd));
+       cards[5] = new Card(R.string.happy, R.string.easter, R.drawable.easter, getResources().getColor(R.color.gradientColorEnd));
+       cards[6] = new Card(R.string.happy, R.string.mother, R.drawable.mother, getResources().getColor(R.color.white));
+       cards[7] = new Card(R.string.happy, R.string.father, R.drawable.fathers, getResources().getColor(R.color.black));
+        cards[8] = new Card(R.string.happy, R.string.independence, R.drawable.independence, getResources().getColor(R.color.colorPrimaryDark));
+       cards[9] = new Card(R.string.happy, R.string.halloween, R.drawable.halloween, getResources().getColor(R.color.colorPrimaryLight));
+       cards[10] = new Card(R.string.happy, R.string.thanks, R.drawable.thanks, getResources().getColor(R.color.colorPrimaryDark));
+       cards[11] = new Card(R.string.merry, R.string.christmas, R.drawable.christmas, getResources().getColor(R.color.colorPrimaryDark));
+   }
 
     private void setCard(Card card){
         backgroundImage.setImageResource(card.getImageResourceId());
