592c358492f4d3564069431d7b5b370974f5e3a3
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-18 08:43:07 -0500

    Remove method to overide onBackPressed.

--- app/src/main/java/com/example/android/occasioncards/CardActivity.java
+++ app/src/main/java/com/example/android/occasioncards/CardActivity.java
@@ -8,17 +8,15 @@ import android.widget.TextView;
 
 public class CardActivity extends AppCompatActivity {
 
+    //declare array of Card objects
+    Card[] cards = new Card[12];
     //creates variable for each view that will change
     private ImageView backgroundImage;
     private TextView greetingText1;
     private TextView greetingText2;
     private TextView youText;
-
     //variable to store users card choice
     private int cardChoice;
-
-    //declare array of Card objects
-    Card[] cards = new Card[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +38,7 @@ public class CardActivity extends AppCompatActivity {
         generateCards();
 
         //switch statement to call set card for correct card from user choice
-        switch (cardChoice){
+        switch (cardChoice) {
             case 1:
                 setCard(cards[0]);
                 break;
@@ -80,32 +78,24 @@ public class CardActivity extends AppCompatActivity {
         }
     }
 
-    //method to reset spinner to "choose card"
-    @Override
-    public void onBackPressed() {
-        Intent cardIntent = new Intent(CardActivity.this, MainActivity.class);
-        cardIntent.putExtra("cardChoice", 0);
-        startActivity(cardIntent);
-   }
-
-   //mathod to create each Card object in card array
-   private void generateCards(){
-       cards[0] = new Card(R.string.happy, R.string.birthday, R.drawable.donut, getResources().getColor(R.color.gradientColorEnd));
-       cards[1] = new Card(R.string.happy, R.string.year, R.drawable.newyear, getResources().getColor(R.color.white));
-       cards[2] = new Card(R.string.happy, R.string.groundhog, R.drawable.groundhog, getResources().getColor(R.color.black));
-       cards[3] = new Card(R.string.happy, R.string.valentine, R.drawable.valentine, getResources().getColor(R.color.red));
-       cards[4] = new Card(R.string.happy, R.string.patrick, R.drawable.patrick, getResources().getColor(R.color.black));
-       cards[5] = new Card(R.string.happy, R.string.easter, R.drawable.easter, getResources().getColor(R.color.gradientColorEnd));
-       cards[6] = new Card(R.string.happy, R.string.mother, R.drawable.mother, getResources().getColor(R.color.white));
-       cards[7] = new Card(R.string.happy, R.string.father, R.drawable.fathers, getResources().getColor(R.color.colorPrimary));
+    //method to create each Card object in card array
+    private void generateCards() {
+        cards[0] = new Card(R.string.happy, R.string.birthday, R.drawable.donut, getResources().getColor(R.color.gradientColorEnd));
+        cards[1] = new Card(R.string.happy, R.string.year, R.drawable.newyear, getResources().getColor(R.color.white));
+        cards[2] = new Card(R.string.happy, R.string.groundhog, R.drawable.groundhog, getResources().getColor(R.color.black));
+        cards[3] = new Card(R.string.happy, R.string.valentine, R.drawable.valentine, getResources().getColor(R.color.red));
+        cards[4] = new Card(R.string.happy, R.string.patrick, R.drawable.patrick, getResources().getColor(R.color.black));
+        cards[5] = new Card(R.string.happy, R.string.easter, R.drawable.easter, getResources().getColor(R.color.gradientColorEnd));
+        cards[6] = new Card(R.string.happy, R.string.mother, R.drawable.mother, getResources().getColor(R.color.white));
+        cards[7] = new Card(R.string.happy, R.string.father, R.drawable.fathers, getResources().getColor(R.color.colorPrimary));
         cards[8] = new Card(R.string.happy, R.string.independence, R.drawable.independence, getResources().getColor(R.color.black));
-       cards[9] = new Card(R.string.happy, R.string.halloween, R.drawable.halloween, getResources().getColor(R.color.orange));
-       cards[10] = new Card(R.string.happy, R.string.thanks, R.drawable.thanks, getResources().getColor(R.color.orange));
-       cards[11] = new Card(R.string.merry, R.string.christmas, R.drawable.christmas, getResources().getColor(R.color.white));
-   }
+        cards[9] = new Card(R.string.happy, R.string.halloween, R.drawable.halloween, getResources().getColor(R.color.orange));
+        cards[10] = new Card(R.string.happy, R.string.thanks, R.drawable.thanks, getResources().getColor(R.color.orange));
+        cards[11] = new Card(R.string.merry, R.string.christmas, R.drawable.christmas, getResources().getColor(R.color.white));
+    }
 
-   //method to set correct values to each view in card
-    private void setCard(Card card){
+    //method to set correct values to each view in card
+    private void setCard(Card card) {
         backgroundImage.setImageResource(card.getImageResourceId());
         greetingText1.setText(card.getStringResourceId1());
         greetingText1.setTextColor(card.getColorResourceId());
